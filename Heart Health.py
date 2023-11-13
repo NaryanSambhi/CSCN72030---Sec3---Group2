38de717e414983441ba5ceed2be9fed1cd2ef292
Nick <npackullmccormi7020@conestogac.on.ca>
2023-11-13 09:58:06 -0500

    Added Heart Health fuctionality

--- Heart Health.py
+++ Heart Health.py
@@ -5,24 +5,71 @@
 
 
 class heart_Health(object):
+    #main variables
+    _heart_rate = 0
+    _blood_oxegen = 0
+    _blood_pressure = 0
+
+    #flag variables
+    _hr_flag = False
+    _bo_flag = False
+    _bp_flag = False
+
+    #internal variables
+    _hrUpperLim = 100
+    _hrLowerLim = 60
+    _boUpperLim = 99
+    _boLowerLim = 95
+    _bpUpperLim = 120
+    _bpLowerLim = 90
+
+    #Constructor
+    def __init__(self, heart_rate, blood_oxegen, blood_pressure):
+        self._heart_rate = heart_rate
+        self._blood_oxegen = blood_oxegen
+        self._blood_pressure = blood_pressure
+        self.checkHRFlag(self)
+        self.checkBOFlag(self)
+        self.checkBPFlag(self)
+    
+
+    #Getters and Setters
+    def get_hr(self):
+        return self._heart_rate
+    def set_hr(self, heart_rate):
+        self._heart_rate = heart_rate
+
+    def get_bo(self):
+        return self._blood_oxegen
+    def set_bo(self, blood_oxegen):
+        self._blood_oxegen = blood_oxegen
+
+    def get_hr(self):
+        return self._blood_pressure
+    def set_hr(self, blood_pressure):
+        self._blood_pressure = blood_pressure
+
+    #setFlags
+    def checkHRFlag(self):
+        if self._heart_rate > self._hrUpperLim or self._heart_rate < self._hrLowerLim:
+            self._hr_flag = True
+        else:
+            self._hr_flag = False
+
+    def checkBOFlag(self):
+        if self._blood_oxegen > self._boUpperLim or self._blood_oxegen < self._boLowerLim:
+            self._bo_flag = True
+        else:
+            self._bo_flag = False
+
+    def checkBPFlag(self):
+        if self._blood_pressure > self._bpUpperLim or self._blood_pressure < self._bpLowerLim:
+            self._bp_flag = True
+        else:
+            self._bp_flag = False
+
     def Display():
       print("This is the heart health info")
-
-
-class heart_Rate(object):
-  def __init__(self, heart_rate):
-    self.heart_rate = heart_rate
-
-
-class blood_Oxygen(object):
-   def __init__(self, blood_oxygen):
-    self.blood_oxygen = blood_oxygen
-
-
-
-class blood_Pressure(object):
-   def __init__(self, blood_pressure):
-    self.blood_pressure = blood_pressure
 
 
 
