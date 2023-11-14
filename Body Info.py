35431e2b0528674ca5c3a150b7e6b99fe1f332c1
umang <umohini4648@conestogac.on.ca>
2023-11-13 20:21:45 -0500

    updated body info

--- Body Info.py
+++ Body Info.py
@@ -5,36 +5,52 @@
 #This class will have 2 smaller classes called FLuid Intake and Body Temperature 
 
 class body_Info(object):
+     #main variables
+    _body_temp = 0
+    _fluid_intake = 0
+
+    #flag variables
+    _body_temp_flag = False
+    _fluid_intake_flag = False
+
+    #internal variables -> change the values 
+    _body_temp_UpperLim = 100.4
+    _body_temp_LowerLim = 97.0
+    _fluid_intake_UpperLim = 5.0
+    _fluid_intake_LowerLim = 0.0
+
+    #constructor
+    def __init__(self, _fluid_intake, _body_temp):
+     self._fluid_intake = _fluid_intake
+     self._body_temp = _body_temp
+     self.body_temp_Flag(self)
+     self.fluid_intake_Flag(self)
+        
+    #Functions
+    def set_fluid_intake(_fluid_intake, input):
+      _fluid_intake = input("How much water have you had today?") 
+    
+    def get_fluid_intake(self):
+      return self._fluid_intake
+  
+    def get_body_temp(self, input):
+      return self.temp
+    
+    def set_body_temp(self):
+      temp = input("What is temperature currently?")
+  
     def see_body_info():
       print("This is the body info")
       
-      fluid = fluid_Intake()
-      intake1 = fluid.get_fluid_intake
-      
-      temp = body_Temp()
-      intake2 = temp.get_body_temp
-
-class fluid_Intake(object):
-  def __init__(self, fluid):
-    self.fluid = fluid
+    #setFlags
+    def body_temp_Flag(self):
+      if self.get_body_temp >= self.body_temp_UpperLim or self.get_body_temp <= self._body_temp_LowerLim:
+          self._body_temp_flag = True
+      else:
+          self._body_temp_flag = False
     
-  #Functions
-  def set_fluid_intake(fluid, input):
-    fluid = input("How much water have you had today?")
-    
-  def get_fluid_intake(self):
-    return self.fluid
-
-class body_Temp(object):
-  def __init__(self, temp):
-    self.temp = temp
-    
-  #Functions
-  def get_body_temp(self, input):
-    temp = input("What is temperature currently?")
-    
-  def set_body_temp(self):
-    return self.temp
-
-# find a way to link it to userdata
-
+    def fluid_intake_Flag(self):
+      if self.get_fluid_intake >= self._fluid_intake_UpperLim or self.get_fluid_intake <= self._body_temp_LowerLim:
+          self. _fluid_intake_flag = True
+      else:
+          self. _fluid_intake_flag = False
